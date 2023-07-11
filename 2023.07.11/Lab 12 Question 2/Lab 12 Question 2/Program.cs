07539bcca8a8b3f8e67c79224e5e389bbc124500
osada2023 <135573064+osada2023@users.noreply.github.com>
2023-07-11 11:27:21 +0530

    Add files via upload

---
+++ 2023.07.11/Lab 12 Question 2/Lab 12 Question 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12_Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            double num1=double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double num2=double.Parse(Console.ReadLine());   
+
+            Arithmetic objarith = new Arithmetic(); 
+            objarith.setAns(num1,num2);
+
+            Console.WriteLine("sum: "+ objarith.Summation());
+            Console.WriteLine("sub: " + objarith.Difference());
+            Console.WriteLine("Multi: " + objarith.Multiplication());
+            Console.WriteLine("Div: " + objarith.Division());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2023.07.11/Lab 12/Lab 12/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12
+{
+    internal class Employee
+    {
+        private int empid;
+        private string empname;
+        private string empdesignation;
+        public void setval1(int ID) 
+        {
+            empid = ID;
+
+        }
+        public void setval2(string Name)
+        {
+            empname = Name;
+        }
+        public void setval3(string Designation)
+        {
+            empdesignation = Designation;
+        }
+
+        public int getval1()
+        {
+            return empid;
+        }
+        public string getval2()
+        {
+            return empname;
+        }
+        public string getval3()
+        {
+            return empdesignation;
+        }
+
+
+
+
+    }
+
+    
+}

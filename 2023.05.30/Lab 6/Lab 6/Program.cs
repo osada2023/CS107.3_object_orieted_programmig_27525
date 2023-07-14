62b0bff0228269e42efef387b84668e79335c836
osada2023 <135573064+osada2023@users.noreply.github.com>
2023-07-14 12:34:25 +0530

    Add files via upload

---
+++ 2023.05.30/Lab 6/Lab 6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, tot = 0, i = 0;
+            double avg;
+            for (i = 0; i <= 9; i++)
+            {
+                Console.Write("Enter the " + (i + 1) + " number: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                tot = tot + num;
+            }
+            avg = (double)tot / 10.0;
+            Console.WriteLine("The average is " + avg);
+            Console.ReadKey();
+
+            int j = 10;
+            for (i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+
+                Console.ReadKey();
+
+
+
+
+            }
+        }
+    }
+
+    }
+
---
+++ 2023.05.30/Lab6 Question 2/Lab6 Question 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab6_Question_2
+{
+    internal class Program
+    {
+        Public static void printname(string name, int age)
+        {
+            Console.WriteLine(name + " " + age);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name; ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age; ");
+            int age = int.Parse(Console.ReadLine());
+
+            printname(name, age);
+            Console.ReadKey();
+
+           
+
+
+        }
+    }
+}

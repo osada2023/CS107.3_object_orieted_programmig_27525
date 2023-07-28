8535228d2d38c23f9002c3ee7fc2131bacf8bae1
osada2023 <135573064+osada2023@users.noreply.github.com>
2023-07-28 10:08:29 +0530

    Add files via upload

---
+++ 2023.07.04/labday11/labday11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labday11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arsize;
+            Console.Write("Enter the size of the array: ");
+            arsize = Convert.ToInt32(Console.ReadLine());
+
+            TheOtherClass obj1 = new TheOtherClass();
+            obj1.Thearray(arsize);
+
+            
+        }
+    }
+}
---
+++ 2023.07.04/labday11/labday11/TheOtherClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labday11
+{
+    internal class TheOtherClass
+    {
+        public int Thearray(int arsize)
+        {
+            int[] arr = new int[arsize];
+
+            int i = 0;
+            for (i = 0; i < arsize; i++)
+            {
+                Console.Write("Enter the element " + (i + 1) + ": ");
+                arr[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The data that you entered are,");
+            for(i=0;i < arr.Length; i++)
+            {
+                if(i==0 || i%2==0)
+                    Console.WriteLine(arr[i]);
+                else
+                    Console.WriteLine("0");
+            }
+            Console.ReadKey();
+            return arr[arsize];
+        }
+    }
+}
